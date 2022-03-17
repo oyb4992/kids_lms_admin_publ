dd8a0ee19be0f7715d4478c56ef142c2125a6c7f
iloveisyou <iloveisyou@hanmir.com>
2022-03-17 22:07:31 +0900

    Updates

--- assets/js/style.js
+++ assets/js/style.js
@@ -4,3 +4,11 @@ function getUrlParam(name) {
         results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
+$(document).ready(function() {
+    if(getUrlParam("iframeId")){
+        if(getUrlParam("iframeId")=="curriculum_form" || getUrlParam("iframeId")=="curriculum_setting" || getUrlParam("iframeId")=="parents_info" || getUrlParam("iframeId")=="parents_banner" || getUrlParam("iframeId")=="pet_setting" || getUrlParam("iframeId")=="pet_list" || getUrlParam("iframeId")=="phrase_setting"){
+            $("#wrap_container").attr("src", "./html/lms/" + getUrlParam("iframeId") + ".html");
+        }   
+    }
+});
